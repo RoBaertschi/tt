0fabececd0ffc23fed939786eab2d4a55bbb2f17
Robin <rtmbaertschi007@gmail.com>
2025-01-21 06:07:33 +0100

    begin ir emit

--- ttir/emit.go
+++ ttir/emit.go
@@ -1,1 +1,15 @@
 package ttir
+
+import "robaertschi.xyz/robaertschi/tt/tast"
+
+func EmitProgram(program *tast.Program) *Program {
+
+}
+
+func emitFunction(function *tast.FunctionDeclaration) *Function {
+
+}
+
+func emitExpression(expr *tast.Expression) (Operand, []Instruction) {
+    
+}
