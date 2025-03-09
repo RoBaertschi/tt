70cfdb5e95b559f26c07a64fa5daea67b0d8adde
Robin Bärtschi <rtmbaertschi007@gmail.com>
2025-03-09 12:18:58 +0100

    extracted the nix package into it's own package

---
+++ nix/package.nix
@@ -0,0 +1,20 @@
+# ml2 ts=2 sts=2 sw=2
+{buildGoModule, version}: buildGoModule {
+  pname = "tt";
+  inherit version;
+  # In 'nix develop', we don't need a copy of the source tree
+  # in the Nix store.
+  src = ./.;
+
+  # This hash locks the dependencies of this package. It is
+  # necessary because of how Go requires network access to resolve
+  # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
+  # details. Normally one can build with a fake hash and rely on native Go
+  # mechanisms to tell you what the hash should be or determine what
+  # it should be "out-of-band" with other tooling (eg. gomod2nix).
+  # To begin with it is recommended to set this, but one must
+  # remember to bump this hash when your dependencies change.
+  # vendorHash = pkgs.lib.fakeHash;
+
+  vendorHash = "sha256-cpjMZr+kP+XyXRxK4YI/lGHl97IROFVVz8DrID0vi6E=";
+}
